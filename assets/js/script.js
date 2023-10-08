e1f92833b35992cb16859a633a145b40daad23f2
Frank Trapani <yo_paisan@yahoo.com>
2023-10-08 10:09:43 -0700

    worked out a lot of the logic needed to make this app function in the jS, but still not all the way there...

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,58 +26,91 @@ THEN the password is either displayed in an alert or written to the page
 var pGenBtn = document.querySelector("#generate");
 
 
-/*---------
-[❗] WHEN prompted for password criteria
-THEN I select which criteria to include in the password
----------*/
-// n/a - this is assumed as part of the other criteria logic that is to follow
-
-
-/*---------
-[❗] WHEN **🗝️prompted** for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
----------*/
-var LengthMsg = prompt("Your password needs to be at least 8 characters in length, but not more than 128. Please try again.");
-
 
 /*---------
 [❗] WHEN asked for character types to include in the password
 THEN I **🗝️confirm** whether or not to include lowercase, uppercase, numeric, and/or special characters
 ---------*/
-var LcaseList    = XXXXXXX["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var LcaseMsg   = confirm("Your password needs a LOWERCASE letter. Please try again.");
+var LcaseList    = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+                    "n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var UcaseList    = XXXXXXX["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var UcaseMsg   = confirm("Your password needs an UPPERCASE letter. Please try again.");
+var UcaseList    = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+                    "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var NumList      = XXXXXXX["0","1","2","3","4","5","6","7","8","9"]
-var NumMsg     = confirm("Your password needs a NUMBER letter. Please try again.");
+var NumList      = ["0","1","2","3","4","5","6","7","8","9"];
 
-var specialList    = XXXXXXX["~","!","@","#","$","%","^","&","*","(",")","-","+","="," ","[","]","<",">","?","/",";",":","Y","Z"]
-var SpecialMsg = confirm("Your password needs a SPECIAL CHARACTER letter. Please try again.");
+// Special Chars Source:  https://owasp.org/www-community/password-special-characters
+// (???) as a string ==>     " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~" 
+var specialList  = [' ', '!', '#', '$', '%', '&', '\\', '(', ')', '*', '+', '-', '.', '/',
+                    ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}', '~'];
 
+var combinedCharList =[];
+var confirmCharList=[];
+var finalPwrdList =[];
 
 /*---------
 [❗] WHEN I answer each prompt
 THEN my input should be **🗝️validated** and at least one character type should be selected
 ---------*/
-function valLength() {
-  // Task logic here
-}
 
-// maybe 1 function for both cases (?)
-function valCase() {
-  // Task logic here
-}
+function generatePassword() {
 
-function valNum() {
-  // Task logic here
-}
+  /*---------
+  [❗] WHEN **🗝️prompted** for the length of the password
+  THEN I choose a length of at least 8 characters and no more than 128 characters
+  ---------*/
+  var LengthMsg = prompt("Please enter a length for your password between 8 and 128 characters.");
+  if (LengthMsg < 8 ) {
+      alert("Your password is not 8 characters long, please lengthen your password.");
+      return null;
+  }
+  if (LengthMsg > 128 ) {
+      alert("Your password is more than 128 characters long, please shorten your password.");
+      return null;
+  }
 
-function valSpecial() {
-  // Task logic here
-}
+  /*---------
+  [❗] WHEN prompted for password criteria
+  THEN I select which criteria to include in the password
+  ---------*/
+  if (isNaN(LengthMsg)) {
+      alert("You need to use a number in your password.");
+      return null;
+  }
 
+  var LcaseMsg     = confirm("Would you like to use a LOWERCASE letter.");
+  var UcaseMsg     = confirm("Would you like to use an UPPERCASE letter.");
+  var NumMsg       = confirm("Would you like to use a NUMBER letter.");
+  var SpecialMsg   = confirm("Would you like to use a SPECIAL CHARACTER letter");
+  
+  if (LcaseMsg === false && UcaseMsg === false && NumMsg === false && SpecialMsg === false){
+    alert("Please select ok for at least one character set so that a password can be generated for you. Please click on the Generate Password button and try again.");
+    return null;
+  }
+
+  if (LcaseMsg) {
+    confirmCharList += combinedCharList.concat(LcaseList);
+  }
+
+  if (UcaseMsg) {
+    confirmCharList += combinedCharList.concat(UcaseList);
+  }
+
+  if (NumMsg) {
+    confirmCharList += combinedCharList.concat(NumList);
+  }
+
+  if (SpecialMsg) {
+    confirmCharList += combinedCharList.concat(specialList);
+  }
+  alert(confirmCharList);
+
+  // LengthMsg for loop & push in to list and write
+  for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  }
+
+}
 
 /*---------
 [❗] WHEN all prompts are answered
@@ -95,9 +128,6 @@ function writePassword() {
 [❗] WHEN the password is generated
 THEN the password is either **🗝️displayed in an alert** or **🗝️written to the page**
 ---------*/
-function displayPassword() {
-  // Task logic here
-}
 
 
 /*---------
