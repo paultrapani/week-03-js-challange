5d11984e1e263e64ab7eecf201943f1a860a073c
Frank Trapani <yo_paisan@yahoo.com>
2023-10-07 18:13:05 -0700

    Updated the README, styles, and JS code (work still in progres)

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 
 Write a Password Generator in JavaScript
 
-
 ## Description 📄| ℹ️
 
 The purpose of this repo project is to create an application that enables employees to generate random passwords based on criteria that they've selected. This app is meant to be run in the browser and features dynamically updated HTML and CSS powered by JavaScript. It has a clean and polished responsive user interface that adapts to multiple screen sizes.
 
+## Source Code Notes
+
+A Project Starter Code package was used for HTML and CSS and partial JavaScript, which I downloaded from the Module 3 Challenge site on UCD Bootcamp Spot.  I have updated the HTML & CSS to make the design my own new unique version, and aside from the starter JavaScript code I wrote the rest myself by figuring it out step by step.  The script is still not fully functional yet.  I'm working so hard to get it done and working.
 
 ## Git Hub Repository URL 🔗
 https://github.com/paultrapani/week-03-js-challange/
 
-
 ## Git Pages Deployment URL 🔗
 https://paultrapani.github.io/week-03-js-challange/
-
 
 ## Mock-Up 🖼️
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,89 @@
-//  My Script
+/*--------- Acceptance Criteria ----------
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
 
-// Write password to the #password input
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+--------- Acceptance Criteria ----------*/
+
+// (Starter Code)
+var pGenBtn = document.querySelector("#generate");
+
+
+/*---------
+[❗] WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+---------*/
+// n/a - this is assumed as part of the other criteria logic that is to follow
+
+
+/*---------
+[❗] WHEN **🗝️prompted** for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+---------*/
+var pwLengthMsg = prompt("Your password needs to be at least 8 characters in length, but not more than 128. Please try again.");
+
+
+/*---------
+[❗] WHEN asked for character types to include in the password
+THEN I **🗝️confirm** whether or not to include lowercase, uppercase, numeric, and/or special characters
+---------*/
+var pwLcaseMsg   = confirm("Your password needs a LOWERCASE letter. Please try again.");
+var pwUcaseMsg   = confirm("Your password needs an UPPERCASE letter. Please try again.");
+var pwNumMsg     = confirm("Your password needs a NUMBER letter. Please try again.");
+var pwSpecialMsg = confirm("Your password needs a SPECIAL CHARACTER letter. Please try again.");
+
+
+/*---------
+[❗] WHEN I answer each prompt
+THEN my input should be **🗝️validated** and at least one character type should be selected
+---------*/
+
+
+/*---------
+[❗] WHEN all prompts are answered
+THEN a password is **🗝️generated** that matches the selected criteria
+---------*/
+
+
+/*---------
+[❗] WHEN the password is generated
+THEN the password is either **🗝️displayed in an alert** or **🗝️written to the page**
+---------*/
+
+// (Starter Code)
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+/*---------
+[❗] WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+---------*/
+
+// This is an **🗝️event listener** for the generate button on click run function "writePassword" 
+
+// (Starter Code)
+
+ pGenBtn.addEventListener("click", writePassword);
+
+// alt idea (?) --> pGenBtn.onclick = writePassword;
