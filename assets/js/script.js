7e83b21a8fb00d442ada7b6d7b06db80aed3c98c
Frank Trapani <yo_paisan@yahoo.com>
2023-10-08 08:32:54 -0700

    Updated styles to remove background color causing white line issues, pwrd message in index page, and additional variables and logic placeholders to js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,8 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 --------- Acceptance Criteria ----------*/
 
-// (Starter Code)
+// #### Starter Code ####
+
 var pGenBtn = document.querySelector("#generate");
 
 
@@ -36,42 +37,66 @@ THEN I select which criteria to include in the password
 [❗] WHEN **🗝️prompted** for the length of the password
 THEN I choose a length of at least 8 characters and no more than 128 characters
 ---------*/
-var pwLengthMsg = prompt("Your password needs to be at least 8 characters in length, but not more than 128. Please try again.");
+var LengthMsg = prompt("Your password needs to be at least 8 characters in length, but not more than 128. Please try again.");
 
 
 /*---------
 [❗] WHEN asked for character types to include in the password
 THEN I **🗝️confirm** whether or not to include lowercase, uppercase, numeric, and/or special characters
 ---------*/
-var pwLcaseMsg   = confirm("Your password needs a LOWERCASE letter. Please try again.");
-var pwUcaseMsg   = confirm("Your password needs an UPPERCASE letter. Please try again.");
-var pwNumMsg     = confirm("Your password needs a NUMBER letter. Please try again.");
-var pwSpecialMsg = confirm("Your password needs a SPECIAL CHARACTER letter. Please try again.");
+var LcaseList    = XXXXXXX["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var LcaseMsg   = confirm("Your password needs a LOWERCASE letter. Please try again.");
+
+var UcaseList    = XXXXXXX["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var UcaseMsg   = confirm("Your password needs an UPPERCASE letter. Please try again.");
+
+var NumList      = XXXXXXX["0","1","2","3","4","5","6","7","8","9"]
+var NumMsg     = confirm("Your password needs a NUMBER letter. Please try again.");
+
+var specialList    = XXXXXXX["~","!","@","#","$","%","^","&","*","(",")","-","+","="," ","[","]","<",">","?","/",";",":","Y","Z"]
+var SpecialMsg = confirm("Your password needs a SPECIAL CHARACTER letter. Please try again.");
 
 
 /*---------
 [❗] WHEN I answer each prompt
 THEN my input should be **🗝️validated** and at least one character type should be selected
 ---------*/
+function valLength() {
+  // Task logic here
+}
+
+// maybe 1 function for both cases (?)
+function valCase() {
+  // Task logic here
+}
+
+function valNum() {
+  // Task logic here
+}
+
+function valSpecial() {
+  // Task logic here
+}
 
 
 /*---------
 [❗] WHEN all prompts are answered
 THEN a password is **🗝️generated** that matches the selected criteria
 ---------*/
-
-
-/*---------
-[❗] WHEN the password is generated
-THEN the password is either **🗝️displayed in an alert** or **🗝️written to the page**
----------*/
-
-// (Starter Code)
+// #### Starter Code ####
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+}
+
+/*---------
+[❗] WHEN the password is generated
+THEN the password is either **🗝️displayed in an alert** or **🗝️written to the page**
+---------*/
+function displayPassword() {
+  // Task logic here
 }
 
 
@@ -82,8 +107,20 @@ THEN I am presented with a series of prompts for password criteria
 
 // This is an **🗝️event listener** for the generate button on click run function "writePassword" 
 
-// (Starter Code)
+// #### Starter Code ####
 
  pGenBtn.addEventListener("click", writePassword);
 
 // alt idea (?) --> pGenBtn.onclick = writePassword;
+
+
+
+
+
+
+
+
+
+
+
+
